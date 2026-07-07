346c68b0215b9b58ae95efce635bbb60aa9dfccb
souma9830 <prabirkumarshee8@gmail.com>
2026-07-07 22:08:21 +0530

    Update import paths in card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/src/lib/utils/cn';
+import { cn } from '@/lib/utils/cn';
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
