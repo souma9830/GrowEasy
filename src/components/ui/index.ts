4185cf900ed59ea369a75f9598c1c3349033dde5
souma9830 <prabirkumarshee8@gmail.com>
2026-07-07 21:03:34 +0530

    Add component index.ts for the user interface

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './button';
+export { Card, CardHeader, CardBody, CardFooter } from './card';
+export { Badge } from './badge';
+export { Input } from './input';
+export { Divider } from './divider';
+export { Skeleton } from './skeleton';
+export { EmptyState, ErrorState } from './states';
