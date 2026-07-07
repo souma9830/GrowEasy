b9888c8772ebe847eabd425847ee5f9773305282
souma9830 <prabirkumarshee8@gmail.com>
2026-07-07 22:08:22 +0530

    Update import paths in states.tsx

--- src/components/ui/states.tsx
+++ src/components/ui/states.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/src/lib/utils/cn';
+import { cn } from '@/lib/utils/cn';
 import { AlertCircle, FileQuestion } from 'lucide-react';
 import { Button } from './button';
 
